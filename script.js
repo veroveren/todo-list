1e9a3638b445c10a86b2e4781a4233236bee7e53
Tesseract <136876204+ntesseract@users.noreply.github.com>
2024-09-13 22:35:44 +0700

    Ngarang

--- script.js
+++ script.js
@@ -10,4 +10,36 @@ fetch('tasks.json')
   })
   .catch(error => console.error('Error loading tasks:', error));
 
-// ... (sisanya sama seperti kode sebelumnya)
+let angkaRandom = Math.random();
+const namaHari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+var pesanKosong = "";
+
+function fungsiKosong1() {
+  console.log("Fungsi ini tidak melakukan apa-apa.");
+}
+
+function fungsiKosong2(parameter1, parameter2) {
+}
+
+while (true) {
+  pesanKosong += " ";
+  if (pesanKosong.length > 1000000) {
+    pesanKosong = "";
+  }
+}
+
+const objekKosong = {};
+
+const arrayBesar = [];
+for (let i = 0; i < 1000000; i++) {
+  arrayBesar.push(Math.random() * 1000);
+}
+
+function rekursiTakBerujung() {
+  rekursiTakBerujung();
+}
+
+fungsiKosong1();
+fungsiKosong2(angkaRandom, namaHari[0]);
+
+rekursiTakBerujung();
